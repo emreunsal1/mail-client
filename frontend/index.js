f31ad803ca10e423c0288c8b6a8e0336e4af370c
Emre Ünsal <unsalemre111@gmail.com>
2023-01-14 19:16:41 +0300

    initial commit

---
+++ frontend/index.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path')
+
+
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  })
+
+  win.loadFile('index.html')
+  win.webContents.openDevTools();
+}
+
+
+app.whenReady().then(()=>{
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
