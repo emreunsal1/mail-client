1219ad97d9d3a2b899ec320864c967187401657c
Emre Ünsal <unsalemre111@gmail.com>
2023-01-17 01:01:15 +0300

    get unread mail to gmail

--- backend/services/mail.js
+++ backend/services/mail.js
@@ -1,25 +1,40 @@
-const {getMailWithImap} = require("../utils");
+const { getMailWithImap } = require("../utils");
 
-const getAllMailList = async  (req,res)=>{
- try {
-  const { email, password} = req.headers;
-  const response = await getMailWithImap(email,password);
-  console.log("abc response", response);
-  res.send("succes");
-  
- } catch (error) {
-  console.log("abc err", error);
-  res.send("not success");
- }
-}
-const mailListUnseen = (req, res)=>{}
+const getAllMailList = async (req, res) => {
+  try {
+    const { email, password } = req.headers;
+    const response = await getMailWithImap(email, password);
+    console.log("abc response", response);
+    res.send("succes");
+  } catch (error) {
+    console.log("abc err", error);
+    res.send("not success");
+  }
+};
+const mailListUnseen = async (req, res) => {
+  try {
+    console.log("abc geldi abi", req.headers);
+    const { email, password } = req.headers;
+    const response = await getMailWithImap(email, password, "*", [
+      ["X-GM-RAW", "has:unread"],
+    ]);
+    res.send("success");
+  } catch (err) {
+    console.log(err);
+    res.send(err);
+  }
+};
 
-const mailListSpecifiedDate = (req, res)=>{}
+const mailListSpecifiedDate = (req, res) => {};
 
-const mailDetail = (req, res)=>{}
+const mailDetail = (req, res) => {};
 
-const sendMail = (req, res)=>{}
+const sendMail = (req, res) => {};
 
-
-
-module.exports = { getAllMailList, mailListUnseen, mailListSpecifiedDate, mailDetail, sendMail };
+module.exports = {
+  getAllMailList,
+  mailListUnseen,
+  mailListSpecifiedDate,
+  mailDetail,
+  sendMail,
+};
--- backend/utils.js
+++ backend/utils.js
@@ -1,78 +1,58 @@
-var Imap = require('imap');
-const simpleParser = require('mailparser').simpleParser;
+var Imap = require("imap");
+const simpleParser = require("mailparser").simpleParser;
 
-
-const connectImap = (email,password)=>{
+const connectImap = (email, password) => {
   var imap = new Imap({
     user: email,
     password: password,
-    host: 'imap.gmail.com',
+    host: "imap.gmail.com",
     port: 993,
     tls: true,
     tlsOptions: {
-      rejectUnauthorized: false
-    }
+      rejectUnauthorized: false,
+    },
   });
   return imap;
-}
+};
 
-
-
-const getMailWithImap = (email, password, count = "*")=>{
+const getMailWithImap = (email, password, count = "*", type = ["ALL"]) => {
   let mailListArray = [];
-  const imap = connectImap(email,password);
-  return new Promise((resolve,reject)=>{
-    imap.once("ready", (err, box)=>{
-      openInbox((err,box)=>{
-        if (err) reject(err);
-        var f = imap.seq.fetch(`1:${count}`, {
-          bodies: '',
-          struct: true
-        });
-        f.on('message', function(msg, seqno) {
-          msg.on('body', function(stream, info) {
-            var buffer = '';
-            simpleParser(stream, (err,mail)=>{
-              if(err) throw new Error(err);
-              const { html, to, from, attachments } = mail;
-              const mailInfoObject = {
-                from: from.value[0],
-                text: mail.text
-              };
-              mailListArray.push(mailInfoObject);
-              resolve(mailListArray);
-            })
-            stream.once('end', function() {
+  const imap = connectImap(email, password);
+  imap.connect();
+  return new Promise((resolve, reject) => {
+    imap.once("ready", () => {
+      imap.openBox("INBOX", true, (err, box) => {
+        imap.search([["X-GM-RAW", "has:attachment"]], (err, results) => {
+          var f = imap.fetch(results, { bodies: "" });
+          f.on("message", function (msg, seqno) {
+            msg.once("attributes", function (attrs) {
+              console.log("attrs.flags", attrs.flags);
+            });
+            msg.on("body", function (stream, info) {
+              simpleParser(stream, (err, mail) => {
+                const { html, to, from, attachments } = mail;
+                const mailInfoObject = {
+                  from: from.value[0],
+                  text: mail.text,
+                };
+                mailListArray.push(mailInfoObject);
+              });
             });
           });
-          msg.once('attributes', function(attrs) {
+          f.once("end", () => {
+            imap.end();
           });
-          msg.once('end', function() {
-          });
         });
-        imap.once('error', function(err) {
-          console.log(err);
-          reject(err);
-        });
-        
-        imap.once('end', function() {
-          console.log('Connection ended');
-        });
-      })
-    
-    })
-    const openInbox = (cb)=>{
-      imap.openBox('INBOX', true, cb);
-    }
-    imap.connect();
-  })
-}
+      });
+    });
+    imap.once("error", function (err) {
+      reject(err);
+    });
 
-
-
-
-
-
-
+    imap.once("end", function () {
+      resolve(mailListArray);
+    });
+  });
+};
 
 module.exports = { connectImap, getMailWithImap };
